f1e05f33f9097fd42d41087014adbab7973da199
Sean Davis <seandavi@gmail.com>
2017-09-24 07:43:38 -0400

    haven't committed in a while

--- init.el
+++ init.el
@@ -2,10 +2,15 @@
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (elpy-enable)
+  ;; (elpy-enable)
   )
 
-(projectile-global-mode)
+
+;; Fix scroll on emacs send-to-console
+;; (setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+
 
 ; recentf mode
 (require 'recentf)
@@ -13,18 +18,310 @@
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-; (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
-; (add-to-list 'auto-mode-alist'("\.markdown\'" . markdown-mode))
-; (add-to-list ’auto-mode-alist'("\.md\'" . markdown-mode))
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\.markdown\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\.md\'" . markdown-mode))
 (require 'poly-R)
 (require 'poly-markdown)
+;;; MARKDOWN
+(add-to-list 'auto-mode-alist '("\\.md$" . poly-markdown-mode))
+
+;;; ORG
+;; org is not working presently
+;; (add-to-list 'auto-mode-alist '("\\.org" . poly-org-mode))
+(setq org-icalendar-timezone "America/New_York")
+
+;;; R related modes
+(add-to-list 'auto-mode-alist '("\\.Snw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd$" . poly-markdown+r-mode))
+(add-to-list 'auto-mode-alist '("\\.rapport$" . poly-rapport-mode))
+(add-to-list 'auto-mode-alist '("\\.Rhtml$" . poly-html+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rbrew$" . poly-brew+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rcpp$" . poly-r+c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cppR$" . poly-c++r-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deeper-blue)))
- '(elpy-rpc-python-command "/Users/sdavis2/.pyenv/shims/python"))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(background-color nil)
+ '(background-mode dark)
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(cursor-color nil)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("4980e5ddaae985e4bae004280bd343721271ebb28f22b3e3b2427443e748cd3f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(ess-indent-with-fancy-comments nil)
+ '(ess-own-style-list
+   (quote
+    ((ess-indent-offset . 2)
+     (ess-offset-arguments . open-delim)
+     (ess-offset-arguments-newline . prev-call)
+     (ess-offset-block . prev-line)
+     (ess-offset-continued . straight)
+     (ess-align-nested-calls "ifelse")
+     (ess-align-arguments-in-calls "function[ 	]*(")
+     (ess-align-continuations-in-calls "[ 	]*(" "if[ 	]*(" "[^ 	]+\\[")
+     (ess-align-blocks if-else)
+     (ess-indent-prev-call-lhs . t)
+     (ess-indent-prev-call-chains . t)
+     (ess-indent-with-fancy-comments . t))))
+ '(ess-set-style Rstudio)
+ '(fci-rule-color "#eee8d5")
+ '(foreground-color nil)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#fdf6e3" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   (quote
+    (("#eee8d5" . 0)
+     ("#B4C342" . 20)
+     ("#69CABF" . 30)
+     ("#69B7F0" . 50)
+     ("#DEB542" . 60)
+     ("#F2804F" . 70)
+     ("#F771AC" . 85)
+     ("#eee8d5" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
+ '(hl-fg-colors
+   (quote
+    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(org-agenda-files (quote ("~/Documents/git/org-files")))
+ '(package-selected-packages
+   (quote
+    (ein yasnippet helm-R helm-aws helm-git helm-projectile go-snippets neotree wanderlust helm helm-core n4js n3-mode solarized-theme noctilux-theme zenburn-theme flx-ido ess ess-smart-equals ess-view ibuffer-projectile nameframe nameframe-projectile org-projectile perspective projectile projectile-codesearch projectile-direnv auctex writeroom-mode workgroups2 w3m snakemake-mode smooth-scrolling smooth-scroll salt-mode projectile-speedbar ppd-sr-speedbar polymode plsql ox-pandoc ox-ioslide ox-impress-js ox-html5slide ox-gfm org2jekyll org-wunderlist org-trello org-toc org-sync org-redmine org-pandoc org-magit org-mac-iCal org-jekyll org-gcal org-caldav org-bullets org-agenda-property org-ac ob-typescript ob-translate ob-swift ob-sml ob-sagemath ob-restclient ob-redis ob-prolog ob-php ob-mongo ob-ml-marklogic ob-lua ob-lfe ob-kotlin ob-ipython ob-http ob-go ob-elixir ob-diagrams ob-cypher ob-coffee ob-browser ob-axiom oauth2 markdown-mode+ magit-tramp magit-svn magit-find-file magit-filenotify magit-annex jsx-mode json-mode js2-refactor jedi isend-mode idomenu gscholar-bibtex groovy-mode graphviz-dot-mode gist geeknote ess-smart-underscore ess-R-object-popup ess-R-data-view eproject elpy deft csv-mode company-restclient company-jedi company-ansible company-anaconda better-defaults ac-js2)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25)
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#c85d17")
+     (60 . "#be730b")
+     (80 . "#b58900")
+     (100 . "#a58e00")
+     (120 . "#9d9100")
+     (140 . "#959300")
+     (160 . "#8d9600")
+     (180 . "#859900")
+     (200 . "#669b32")
+     (220 . "#579d4c")
+     (240 . "#489e65")
+     (260 . "#399f7e")
+     (280 . "#2aa198")
+     (300 . "#2898af")
+     (320 . "#2793ba")
+     (340 . "#268fc6")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+(add-to-list 'auto-mode-alist '("\.Rmd\'" . poly-markdown+r-mode))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;;'(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Monaco")))))
+
+(setq org-todo-keywords
+       '((sequence "TODO" "NEXT" "DONE")))
+
+;; (require 'auto-complete)
+;; (global-auto-complete-mode)
+(require 'auto-complete-config)
+(ac-config-default)
+
+
+(eval-after-load "company"
+ '(progn
+   (add-to-list 'company-backends 'company-anaconda)))
+
+(require 'ess-site)
+(setq ess-use-auto-complete t)
+(setq ac-auto-start 2)
+(setq ac-auto-show-menu 0.2)
+(ess-toggle-underscore nil)
+
+;; (setenv "PATH" (concat (getenv "PATH") ":~/.pyenv/shims/"))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin/"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+;; (setq exec-path (append exec-path '("~/.pyenv/shims")))
+
+
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
+
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'eldoc-mode)
+
+
+;; Org agenda
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(setq org-directory "~/Documents/gin/Notes/")
+(setq org-default-notes-file "~/Documents/git/Notes/Notes.org")
+
+;; I use C-c c to start capture mode
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/Documents/git/Notes/refile.org")
+	       "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+	      ("r" "respond" entry (file "~/Documents/git/Notes/refile.org")
+	       "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+	      ("n" "note" entry (file "~/Documents/git/Notes/refile.org")
+	       "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+	      ("j" "Journal" entry (file+datetree "~/Documents/git/Notes/diary.org")
+	       "* %?\n%U\n" :clock-in t :clock-resume t)
+	      ("w" "org-protocol" entry (file "~/Documents/git/Notes/refile.org")
+	       "* TODO Review %c\n%U\n" :immediate-finish t)
+	      ("m" "Meeting" entry (file "~/Documents/git/Notes/refile.org")
+	       "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+	      ("p" "Phone call" entry (file "~/Documents/git/Notes/refile.org")
+	       "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+	      ("h" "Habit" entry (file "~/Documents/git/Notes/refile.org")
+	                      "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+;; org refile
+; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+; Use full outline paths for refile targets - we file directly with IDO
+(setq org-refile-use-outline-path t)
+
+; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
+
+; Allow refile to create parent tasks with confirmation
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+; Use IDO for both buffer and file completion and ido-everywhere to t
+(setq org-completion-use-ido t)
+(setq ido-everywhere t)
+(setq ido-max-directory-size 100000)
+(ido-mode (quote both))
+; Use the current window when visiting files and buffers with ido
+(setq ido-default-file-method 'selected-window)
+(setq ido-default-buffer-method 'selected-window)
+; Use the current window for indirect buffer display
+(setq org-indirect-buffer-display 'current-window)
+
+;;;; Refile settings
+; Exclude DONE state tasks from refile targets
+(defun bh/verify-refile-target ()
+  "Exclude todo keywords with a done state from refile targets"
+  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+
+(setq org-refile-target-verify-function 'bh/verify-refile-target)
+
+;; active Babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((R . t)
+   (python . t)
+   (org . t)
+   (java . t)
+   (scala . t)
+   (shell . t)
+   (emacs-lisp . nil)
+   ))
+
+(projectile-global-mode)
+(nameframe-projectile-mode t)
+;(require 'projectile-speedbar)
+;(setq projectile-switch-project-action 'neotree-projectile-action)
+
+
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+  (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+  (defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+    (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+    (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+  (add-hook 'ido-setup-hook 'ido-define-keys)
+
+;; WanderLust
+;; wanderlust
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+
+;; IMAP
+(setq elmo-imap4-default-server "imap.gmail.com")
+(setq elmo-imap4-default-user "seandavi@gmail.com") 
+(setq elmo-imap4-default-authenticate-type 'clear) 
+(setq elmo-imap4-default-port '993)
+(setq elmo-imap4-default-stream-type 'ssl)
+
+(setq elmo-imap4-use-modified-utf7 t) 
+
+;; SMTP
+(setq wl-smtp-connection-type 'starttls)
+(setq wl-smtp-posting-port 587)
+(setq wl-smtp-authenticate-type "plain")
+(setq wl-smtp-posting-user "seandavi")
+(setq wl-smtp-posting-server "smtp.gmail.com")
+(setq wl-local-domain "gmail.com")
+
+(setq wl-default-folder "%inbox")
+(setq wl-default-spec "%")
+(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
+(setq wl-trash-folder "%[Gmail]/Trash")
+
+(setq wl-folder-check-async t) 
+
+(setq elmo-imap4-use-modified-utf7 t)
+
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
+(put 'downcase-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,12 +329,8 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'auto-complete)
-(global-auto-complete-mode)
-(require 'auto-complete-config)
-(ac-config-default)
-(require 'ess-site)
-(setq ess-use-auto-complete t)
-(setq ac-auto-start 2)
-(setq ac-auto-show-menu 0.2)
-(ess-toggle-underscore nil)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"                 ;; personal snippets
+        ))
+(require 'yasnippet)
+(yas-global-mode 1)
